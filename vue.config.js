e5e0e602047acd71c70274920b9d270b798b8180
pedro <antonioaren2@gmail.com>
2023-03-02 22:32:03 +0100

    Feat: Adding some format and testing also

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.compilerOptions.whitespace = 'preserve'
+        return options
+      })
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/_mixins.scss";
+        `
+      }
+    }
+  }
 })
